872852797bfc937c6fffd273ef06b4917283ca51
hkurnali <huseyinkurnali@hotmail.com>
2023-04-17 23:10:44 +0300

    Entityler eklendi

---
+++ Edukator.DataAccesLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using Edukator.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.DataAccesLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-H080ONB\\SQLEXPRESS01;initial catalog=DbEdukator;integrated security=true;");
+        }
+        public DbSet<Course> Courses{ get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ContactInfo> CategoryInfos { get; set; }
+    }
+}
---
+++ Edukator.EntityLayer/Concrete/ContactInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edukator.EntityLayer.Concrete
+{
+    public class ContactInfo
+    {
+        public int ContactInfoID { get; set; }
+
+        public string Adress1 { get; set; }
+
+        public string Adress2 { get; set; }
+
+        public string Mail1 { get; set; }
+
+        public string Mail2 { get; set; }
+
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+    }
+}
